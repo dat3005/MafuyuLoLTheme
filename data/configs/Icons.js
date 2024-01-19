724c00a5c2537f49301a9b79a9c3ab23dcb54e91
dat3005 <37260680+dat3005@users.noreply.github.com>
2024-01-19 21:28:46 +0700

    Add files via upload

---
+++ data/configs/Icons.js
@@ -0,0 +1,47 @@
+const data = {
+    //______________________________________________________________________________//
+
+    "Animation-logo": "ElainaFly.gif",
+    "Static-logo"   : "Logo.png",
+
+    "Avatar"        : "avatar.png",
+    "Border"        : "spirit-of-ionia-crests-wild-rift.png",
+    "Hover-card"    : "Hover-card-backdrop.png",
+
+    "RP-icon"       : "ctbezh.png",
+    "BE-icon"       : "2604.png",
+
+    "Rank-icon"     : "Rank.png",
+    "Honor"         : "emblem_5.png",
+    "Trophy"        : "trophy_empty.png",
+    "Class-banner"  : "Clash.png",
+
+    "Ticker"        : "ticker.png",
+    
+    "Mouse-cursor"  : "ElainaDed.png",
+
+    "LL_Settings"   : "LL-Settings.png",
+
+    //Runes
+    "Precision"  : "8000.png",
+    "Domination" : "8100.png",
+    "Sorcery"    : "8200.png",
+    "Resolve"    : "8400.png",
+    "Inspiration": "8300.png",
+
+    //Gamemodes
+    "classic_def"  : "classic-default.png",
+    "classic_act"  : "classic-active.png",
+    "classic_video": "classic-video.webm",
+    "aram_def"     : "aram-default.png",
+    "aram_act"     : "aram-active.png",
+    "aram_video"   : "aram-video.webm",
+    "tft_def"      : "tft-default.png",
+    "tft_act"      : "tft-active.png",
+    "tft_video"    : "tft-video.webm",
+    "cherry_def"   : "cherry-default.png",
+    "cherry_act"   : "cherry-active.png",
+    "cherry_video" : "cherry-video.webm",
+    //______________________________________________________________________________//
+}
+export default data
